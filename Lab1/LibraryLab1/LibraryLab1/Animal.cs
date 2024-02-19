03d78e37bb34bb2335ec61667f0e934e03384340
ZakharovIvan <ipz221_zia@student.ztu.edu.ua>
2024-02-19 18:49:27 +0200

    added abs class Animal and class ZooStaff

---
+++ Lab1/LibraryLab1/LibraryLab1/Animal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryLab1
+{
+    public abstract class Animal
+    {
+        protected string name;
+        protected string vetPassport;
+
+        public Animal(string name, string vetPassport) 
+        { 
+            this.name = name;
+            this.vetPassport = vetPassport;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public string GetVetPassport()
+        {
+            return vetPassport;
+        }
+    }
+}
---
+++ Lab1/LibraryLab1/LibraryLab1/ZooStaff.cs
@@ -0,0 +1,17 @@
+﻿namespace LibraryLab1
+{
+    public class ZooStaff
+    {
+        protected string name;
+        protected string surname;
+        protected string position;
+
+        public ZooStaff(string name, string surname, string position) 
+        { 
+            this.name = name;
+            this.surname = surname;
+            this.position = position;
+        }
+
+    }
+}
