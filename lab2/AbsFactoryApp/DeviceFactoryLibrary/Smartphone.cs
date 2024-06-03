24790ee7bc1fa41f802499029611e51bd033d5ae
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 18:46:53 +0300

    add class Smartphone

---
+++ lab2/AbsFactoryApp/DeviceFactoryLibrary/Smartphone.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceFactoryLibrary
+{
+    public class Smartphone : IDevice
+    {
+        private string _brand;
+
+        public Smartphone(string brand)
+        {
+            _brand = brand;
+        }
+
+        public string GetDeviceDetails()
+        {
+            return $"{_brand} Smartphone";
+        }
+    }
+}
