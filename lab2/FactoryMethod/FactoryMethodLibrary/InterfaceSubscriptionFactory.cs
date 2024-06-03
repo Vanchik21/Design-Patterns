3912b3fab985e9c6cca3aa1747ec6585e4b8b3af
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 17:14:50 +0300

    added class that defines a method that factory classes should implement to create objects of type Subscription

---
+++ lab2/FactoryMethod/FactoryMethodLibrary/InterfaceSubscriptionFactory.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodLibrary
+{
+    public interface ISubscriptionFactory
+    {
+        Subscription CreateSubscription(string type);
+    }
+}
