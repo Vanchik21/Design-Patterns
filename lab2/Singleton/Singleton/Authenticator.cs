f2c3a8de2830d9053cfcd76fa485428674731896
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 19:54:26 +0300

    add class Authenticator

---
+++ lab2/Singleton/Singleton/Authenticator.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public class Authenticator
+    {
+        private static readonly Lazy<Authenticator> instance =
+            new Lazy<Authenticator>(() => new Authenticator());
+
+        protected Authenticator()
+        {
+            if (instanceCreated)
+            {
+                throw new InvalidOperationException("Instance already created.");
+            }
+            instanceCreated = true;
+            Console.WriteLine("Authenticator initialized.");
+        }
+
+        private static bool instanceCreated = false;
+
+        public static Authenticator Instance
+        {
+            get
+            {
+                return instance.Value;
+            }
+        }
+
+        public void Authenticate()
+        {
+            Console.WriteLine("Authentication successful.");
+        }
+    }
+}
