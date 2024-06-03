e2adeb77838395ca1fec56965feada96b0d1c0d2
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 17:08:12 +0300

    add EducationalSubscription class

---
+++ lab2/FactoryMethod/FactoryMethodLibrary/EducationalSubsciption.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodLibrary
+{
+    public class EducationalSubscription : Subscription
+    {
+        public override void GetDetails()
+        {
+            Console.WriteLine("Educational Subscription: $15/month, Min. period: 3 months, Channels: Educational");
+        }
+    }
+}
