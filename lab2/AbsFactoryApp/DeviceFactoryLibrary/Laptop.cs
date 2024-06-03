593ddab838be525f295f63c8f4734d434a6540e0
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 18:27:14 +0300

    added class Laptop

---
+++ lab2/AbsFactoryApp/DeviceFactoryLibrary/Laptop.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeviceFactoryLibrary
+{
+    public class Laptop : IDevice
+    {
+        private string _brand;
+
+        public Laptop(string brand)
+        {
+            _brand = brand;
+        }
+
+        public string GetDeviceDetails()
+        {
+            return $"{_brand} Laptop";
+        }
+    }
+}
