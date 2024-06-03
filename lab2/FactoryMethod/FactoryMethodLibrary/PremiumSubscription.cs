91141bbec0146f0fbdaea0aca2978fd679e7b0d0
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 17:09:54 +0300

    add class PremiumSubscription

---
+++ lab2/FactoryMethod/FactoryMethodLibrary/PremiumSubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodLibrary
+{
+    public class PremiumSubscription : Subscription
+    {
+        public override void GetDetails()
+        {
+            Console.WriteLine("Premium Subscription: $25/month, Min. period: 6 months, Channels: All");
+        }
+    }
+}
