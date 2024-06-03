1e092b16abab335e664631a2940a12cd53b1baa0
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 17:24:33 +0300

    added 3 classes to implement the purchase of a subscription and Program.cs to recheck the implementation of all the previously written code

--- lab2/FactoryMethod/FactoryMethod/Program.cs
+++ lab2/FactoryMethod/FactoryMethod/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using FactoryMethodLibrary;
+using System;
+
+namespace FactoryMethodApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ISubscriptionFactory websiteFactory = new WebSite();
+            ISubscriptionFactory mobileAppFactory = new MobileApp();
+            ISubscriptionFactory managerCallFactory = new ManagerCall();
+
+            Subscription domesticSubscription = websiteFactory.CreateSubscription("Domestic");
+            Subscription educationalSubscription = mobileAppFactory.CreateSubscription("Educational");
+            Subscription premiumSubscription = managerCallFactory.CreateSubscription("Premium");
+
+            domesticSubscription.GetDetails();
+            educationalSubscription.GetDetails();
+            premiumSubscription.GetDetails();
+        }
+    }
+}
---
+++ lab2/FactoryMethod/FactoryMethodLibrary/ManagerCall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodLibrary
+{
+    public class ManagerCall : ISubscriptionFactory
+    {
+        public Subscription CreateSubscription(string type)
+        {
+            switch (type)
+            {
+                case "Domestic":
+                    return new DomesticSubscription();
+                case "Educational":
+                    return new EducationalSubscription();
+                case "Premium":
+                    return new PremiumSubscription();
+                default:
+                    throw new ArgumentException("Invalid subscription type");
+            }
+        }
+    }
+}
