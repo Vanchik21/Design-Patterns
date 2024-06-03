1be7219f4b314680d3456bcefc8672f547e157c2
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 17:05:32 +0300

    add class DomesticSubscription

---
+++ lab2/FactoryMethod/FactoryMethodLibrary/DomesticSubscription.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodLibrary
+{
+    public class DomesticSubscription : Subscription
+    {
+        public override void GetDetails()
+        {
+            Console.WriteLine("Domestic Subscription: $10/month, Min. period: 1 month, Channels: Basic");
+        }
+    }
+}
