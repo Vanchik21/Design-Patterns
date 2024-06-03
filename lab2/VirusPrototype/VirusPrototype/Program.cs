26c64bcb10d5d9357823cef3dbcfc708125fdcd1
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 20:43:21 +0300

    add PrototypePattern Task

---
+++ lab2/VirusPrototype/VirusPrototype/Program.cs
@@ -0,0 +1,32 @@
+﻿using VirusPrototype;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Virus parentVirus = new Virus(2.5, 5, "ParentVirus", "TypeA");
+        Virus childVirus1 = new Virus(1.2, 2, "ChildVirus1", "TypeB");
+        Virus childVirus2 = new Virus(1.0, 1, "ChildVirus2", "TypeC");
+        Virus grandChildVirus = new Virus(0.5, 0, "GrandChildVirus", "TypeD");
+
+        childVirus1.AddChild(grandChildVirus);
+        parentVirus.AddChild(childVirus1);
+        parentVirus.AddChild(childVirus2);
+
+        Virus clonedParentVirus = parentVirus.Clone();
+
+        Console.WriteLine("Original Parent Virus:");
+        DisplayVirusFamily(parentVirus, 0);
+        Console.WriteLine("\nCloned Parent Virus:");
+        DisplayVirusFamily(clonedParentVirus, 0);
+    }
+
+    static void DisplayVirusFamily(Virus virus, int level)
+    {
+        Console.WriteLine(new string('-', level * 2) + virus.ToString());
+        foreach (var child in virus.Children)
+        {
+            DisplayVirusFamily(child, level + 1);
+        }
+    }
+}
---
+++ lab2/VirusPrototype/VirusPrototype/PrototypePattern.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirusPrototype
+{
+    public class Virus
+    {
+        public double Weight { get; set; }
+        public int Age { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public List<Virus> Children { get; set; }
+
+        public Virus(double weight, int age, string name, string species)
+        {
+            Weight = weight;
+            Age = age;
+            Name = name;
+            Species = species;
+            Children = new List<Virus>();
+        }
+
+        public Virus Clone()
+        {
+            Virus clonedVirus = (Virus)this.MemberwiseClone();
+            clonedVirus.Children = new List<Virus>();
+            foreach (Virus child in this.Children)
+            {
+                clonedVirus.Children.Add(child.Clone());
+            }
+            return clonedVirus;
+        }
+
+        public void AddChild(Virus child)
+        {
+            Children.Add(child);
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Species}), Weight: {Weight}, Age: {Age}, Children: {Children.Count}";
+        }
+    }
+}
