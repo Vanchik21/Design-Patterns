032d3754b2a8791c391d8399f4b62aaee28cf614
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-03 19:07:52 +0300

    add Program,cs to test

--- lab2/AbsFactoryApp/AbsFactoryApp/Program.cs
+++ lab2/AbsFactoryApp/AbsFactoryApp/Program.cs
@@ -1,1 +1,23 @@
-﻿
+﻿using System;
+using DeviceFactoryLibrary;
+
+namespace AbsFactoryApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IDeviceFactory iproneFactory = new IProneFactory();
+            IDeviceFactory kiaomiFactory = new KiaomiFactory();
+            IDeviceFactory balaxyFactory = new BalaxyFactory();
+
+            IDevice iproneLaptop = iproneFactory.CreateLaptop();
+            IDevice kiaomiSmartphone = kiaomiFactory.CreateSmartphone();
+            IDevice balaxyEBook = balaxyFactory.CreateEBook();
+
+            Console.WriteLine(iproneLaptop.GetDeviceDetails());
+            Console.WriteLine(kiaomiSmartphone.GetDeviceDetails());
+            Console.WriteLine(balaxyEBook.GetDeviceDetails());
+        }
+    }
+}
