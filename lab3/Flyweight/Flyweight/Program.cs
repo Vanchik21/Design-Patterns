351795fbc73946d06428fa93e83a4b1d1d129301
Zakharov Ivan <ipz221_zia@student.ztu.edu.ua>
2024-06-04 04:20:28 +0300

    ended lab 3.....

---
+++ lab3/Flyweight/Flyweight/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using FlyweightLib;
+using CompositeLib;
+
+namespace YourNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pathForFlyweight = @"C:\Users\ivank\OneDrive\Документы\Design-Patterns\lab3\files\task6.txt";
+
+            try
+            {
+                Console.WriteLine("Without Flyweight");
+                ReadFiles.ReadFilesWithoutFlyweight(pathForFlyweight);
+                Console.WriteLine("----------------------------");
+                Console.WriteLine("With Flyweight");
+                ReadFiles.ReadFilesWithFlyweight(pathForFlyweight);
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine($"An error occurred while accessing the file: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An unexpected error occurred: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ lab3/Flyweight/FlyweightLib/Tag.cs
@@ -0,0 +1,22 @@
+﻿using CompositeLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlyweightLib
+{
+    public class Tag
+    {
+        public string TagName { get; }
+        public int TagDisplayType { get; }
+        public int TagIsPaired { get; }
+        public Tag(string tag, int displayType, int isPaired)
+        {
+            this.TagName = tag;
+            this.TagDisplayType = displayType;
+            this.TagIsPaired = isPaired;
+        }
+    }
+}
